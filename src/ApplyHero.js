0daaa5fc28ad5bddfc5ddac201174c72c96bcec7
Tim Lazarus <me@timlazarus.com>
2018-10-02 08:11:57 +0100

    Remove title

--- src/ApplyHero.js
+++ src/ApplyHero.js
@@ -7,7 +7,6 @@ import './ApplyHero.css';
 
 export default function ApplyHero() {
   return (
-    <div>
       <div className="ApplyHero">
         <div className="ApplyHero__section ApplyHero__section--hac">
           <img src={hacLogo} className="ApplyHero__logo" />
@@ -16,9 +15,5 @@ export default function ApplyHero() {
           <img src={hcLogoPNG} className="ApplyHero__logo" />
         </div>
       </div>
-      <div className="ApplyFooter">
-        <h2>Freshers Fair 2018</h2>
-      </div>
-    </div>
   );
 }
